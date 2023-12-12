a9181666c2b5c3e4ce4e0e965a3c35222dd5d8e1
MatteoPiselli <pro.matteo.piselli@gmail.com>
2023-12-12 11:35:06 +0100

    add leaflet

--- projet-api-qfap/src/App.js
+++ projet-api-qfap/src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import {
+  MapContainer,
+  TileLayer,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <MapContainer
+        style={{
+          height: "515px",
+          width: "1044px",
+          borderRadius: "25px"
+        }}
+        center={[48.866667, 2.333333]}
+        zoom={12}
+      >
+        {/* add google map tile url  */}
+        <TileLayer
+          attribution="Google Maps"
+          url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
+        />
+      </MapContainer>
+    </div>
+  </div>;  
 }
 
 export default App;
