96a3656940beb6b6fe8831b66aa81d2305388b5c
dan-arki <100216647+dan-arki@users.noreply.github.com>
2023-12-12 15:32:37 +0100

    commit Dan 2
    card component added

--- projet-api-qfap/src/components/Card/index.jsx
+++ projet-api-qfap/src/components/Card/index.jsx
@@ -1,13 +1,28 @@
 function Card() {
   return (
-    <div className="flex flex-column justify-around w-40 m-auto">
-      <h3 className="text-base text-primary-blue font-bold bg-blue-600 bg-opacity-30 p-1 px-3 rounded-full">
-        Loisirs
-      </h3>
-      <h2 className="text-red">Exposition Haussmann en capitale</h2>
-      <div className="flex flex-column m-auto">
-        <img src="src/icons/iconCalendar" alt="" />
-        <span>2 novembre - 31 décembre 2023</span>
+    <div className="flex flex-column justify-around m-auto">
+      <div>
+        <img
+          className="w-78 h-96 mb-4 rounded-3xl border-solid border-8 border-primary-blue border-opacity-20"
+          src="images/testImg.jpg"
+          alt=""
+        />
+        <div className="flex flex-col gap-4">
+          <h3 className="text-base text-primary-blue font-bold font bg-primary-blue bg-opacity-10 text-center rounded-full py-2 px-8 w-28">
+            Loisirs
+          </h3>
+          <div className="flex flex-col gap-0.5">
+            <h2 className="text-xl text-textColor font-medium">
+              Exposition Haussmann en capitale
+            </h2>
+            <div className="flex flex-row gap-2">
+              <img src="icons/iconCalendar.svg" alt="" />
+              <span className="text-sm text-secondaryRed font-medium">
+                2 novembre - 31 décembre 2023
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- projet-api-qfap/tailwind.config.js
+++ projet-api-qfap/tailwind.config.js
@@ -1,19 +1,23 @@
-// /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: {
-          blue: "#F0F4F8",
+          blue: "#354ACE",
           blueHover: "#2B3AEB",
         },
-        secondaryRed: "E22C3E",
+        secondaryRed: "#E22C3E",
         textColor: "#071F31",
       },
 
       fontFamily: {
         sans: ["Outfit", "sans-serif"],
+      },
+
+      width: {
+        78: "19.5rem",
       },
 
       plugins: [],
