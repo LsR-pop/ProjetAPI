9b8255b84b539a1ff21fac521daae4e4869b3ad5
dan-arki <100216647+dan-arki@users.noreply.github.com>
2023-12-12 10:37:22 +0100

    first commit Dan
    colors variables added
    font variables added
    icons added
    logo added

--- projet-api-qfap/tailwind.config.js
+++ projet-api-qfap/tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    extend: {
+      colors: {
+        primary: {
+          blue: "#F0F4F8",
+          blueHover: "#2B3AEB",
+        },
+        secondaryRed: "E22C3E",
+        textColor: "#071F31",
+      },
 
+      fontFamily: {
+        sans: ["Outfit", "sans-serif"],
+      },
+
+      plugins: [],
+    },
+  },
+};
