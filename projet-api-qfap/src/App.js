7d6e192e9ac1a45e1927c696b8326dfea8af5267
MatteoPiselli <pro.matteo.piselli@gmail.com>
2023-12-13 09:49:10 +0100

    add filters

--- projet-api-qfap/src/App.js
+++ projet-api-qfap/src/App.js
@@ -5,7 +5,765 @@ import "leaflet/dist/leaflet.css";
 function App() {
   return (
     <div className="App">
-      <div style={{ display: "flex", justifyContent: "center" }}>
+      <p
+        style={{
+          color: "#071F31",
+          fontFamily: "Outfit",
+          fontSize: "32px",
+          fontStyle: "normal",
+          fontWeight: "700",
+          lineHeight: "normal",
+          display: "flex",
+        }}
+      >
+        Filter
+      </p>
+      <div
+        style={{
+          color: "#071F31",
+          fontSize: 16,
+          fontFamily: "Outfit",
+          fontWeight: "600",
+          wordWrap: "break-word",
+          display: "flex",
+        }}
+      >
+        Mots clés
+      </div>
+
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          padding: 10,
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          gap: 16,
+          display: "inline-flex",
+        }}
+      >
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div
+            style={{
+              width: 20,
+              height: 20,
+              background: "#354ACE",
+            }}
+          ></div>
+          <div
+            style={{
+              width: 58,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Danse
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 58,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Théâtre
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div
+            style={{
+              width: 20,
+              height: 20,
+              background: "#354ACE",
+            }}
+          ></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Loisirs
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Photos
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Sport
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          color: "#071F31",
+          fontSize: 16,
+          fontFamily: "Outfit",
+          fontWeight: "600",
+          wordWrap: "break-word",
+          display: "flex",
+        }}
+      >
+        Audience
+      </div>
+
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          padding: 10,
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          gap: 16,
+          display: "inline-flex",
+        }}
+      >
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div
+            style={{
+              width: 20,
+              height: 20,
+              background: "#354ACE",
+            }}
+          ></div>
+          <div
+            style={{
+              width: 58,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Danse
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 58,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Théâtre
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div
+            style={{
+              width: 20,
+              height: 20,
+              background: "#354ACE",
+            }}
+          ></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Loisirs
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Photos
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Sport
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          color: "#071F31",
+          fontSize: 16,
+          fontFamily: "Outfit",
+          fontWeight: "600",
+          wordWrap: "break-word",
+          display: "flex",
+        }}
+      >
+        Tarifs
+      </div>
+
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          padding: 10,
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          gap: 16,
+          display: "inline-flex",
+        }}
+      >
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div
+            style={{
+              width: 20,
+              height: 20,
+              background: "#354ACE",
+            }}
+          ></div>
+          <div
+            style={{
+              width: 58,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Danse
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 58,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Théâtre
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div
+            style={{
+              width: 20,
+              height: 20,
+              background: "#354ACE",
+            }}
+          ></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Loisirs
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Photos
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Sport
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          color: "#071F31",
+          fontSize: 16,
+          fontFamily: "Outfit",
+          fontWeight: "600",
+          wordWrap: "break-word",
+          display: "flex",
+        }}
+      >
+        Dates
+      </div>
+
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          padding: 10,
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          gap: 16,
+          display: "inline-flex",
+        }}
+      >
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div
+            style={{
+              width: 20,
+              height: 20,
+              background: "#354ACE",
+            }}
+          ></div>
+          <div
+            style={{
+              width: 58,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Danse
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 58,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Théâtre
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div
+            style={{
+              width: 20,
+              height: 20,
+              background: "#354ACE",
+            }}
+          ></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Loisirs
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Photos
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Sport
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          color: "#071F31",
+          fontSize: 16,
+          fontFamily: "Outfit",
+          fontWeight: "600",
+          wordWrap: "break-word",
+          display: "flex",
+        }}
+      >
+        Accessibilité
+      </div>
+
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          padding: 10,
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          gap: 16,
+          display: "inline-flex",
+        }}
+      >
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div
+            style={{
+              width: 20,
+              height: 20,
+              background: "#354ACE",
+            }}
+          ></div>
+          <div
+            style={{
+              width: 58,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Danse
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 58,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Théâtre
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div
+            style={{
+              width: 20,
+              height: 20,
+              background: "#354ACE",
+            }}
+          ></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Loisirs
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Photos
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 8,
+            display: "inline-flex",
+          }}
+        >
+          <div style={{ width: 20, height: 20, background: "#354ACE" }}></div>
+          <div
+            style={{
+              width: 54,
+              color: "#354ACE",
+              fontSize: 16,
+              fontFamily: "Outfit",
+              fontWeight: "500",
+              wordWrap: "break-word",
+            }}
+          >
+            Sport
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
         <MapContainer
           style={{
             height: "515px",
