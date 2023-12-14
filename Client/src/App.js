9f3355376cc35af774297beb3565fde9aa909429
Léo Banckaert <94908599+Nocalet@users.noreply.github.com>
2023-12-14 12:53:20 +0100

    base api

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,26 +5,37 @@ import React, { useEffect, useState } from "react";
 function App() {
   const [data, setData] = useState([{}]);
 
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
-    fetch("/api/events")
+    fetch("/api/events?page=" + page + "&limit=" + 9)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setData(data);
       });
-  }, []);
+  }, [page, setPage]);
 
   return (
     <div className="App">
       {data.map((event) => {
         return (
           <div>
-            <h1>{event.title}</h1>
-            {event.tags &&
-              event.tags.map((tag) => <p key={tag.id}>{tag.tag}</p>)}
+            <h1 key={event.id}>{event.title}</h1>
+            {/* {event.tags &&
+              event.tags.map((tag) => <p key={tag.id}>{tag.tag}</p>)} */}
           </div>
         );
       })}
+      {
+        <button
+          style={{ backgroundColor: "red" }}
+          onClick={() => setPage(page + 1)}
+        >
+          next
+        </button>
+      }
+
       <Home></Home>
     </div>
   );
--- Server/index.js
+++ Server/index.js
@@ -12,12 +12,26 @@ const {
   Group,
   Address,
 } = require("./Data/models");
+const e = require("express");
 
 app.use(cors());
 
 app.get("/api/events", (req, res) => {
+  let page = parseInt(req.query.page) || 1;
+  let limit = parseInt(req.query.limit) || 3;
+  let offset = limit * (page - 1);
+
+  let next = "/api/events?page=" + (page + 1) + "&limit=" + limit;
+  let prev = "/api/events?page=" + (page - 1) + "&limit=" + limit;
+  if (page == 1) {
+    prev = null;
+  }
+
+  let count = 0;
+
   Event.findAll({
-    limit: 10,
+    offset: offset,
+    limit: limit,
     include: [
       {
         model: Tag,
@@ -46,7 +60,128 @@ app.get("/api/events", (req, res) => {
       },
     ],
   }).then((events) => {
-    res.json(events);
+    if (events.length < limit) {
+      next = null;
+    }
+    if (events.length < limit) {
+      count += events.length + offset;
+    } else {
+      count += events.length * page;
+    }
+
+    ret = {
+      count: count,
+      next: next,
+      previous: prev,
+      results: events,
+    };
+    res.json(ret);
+  });
+});
+
+app.get("/api/events/:id", (req, res) => {
+  Event.findByPk(req.params.id, {
+    include: [
+      {
+        model: Tag,
+        as: "tags",
+        through: { attributes: [] },
+      },
+      {
+        model: AccessType,
+        as: "accessType",
+      },
+      {
+        model: PriceType,
+        as: "priceType",
+      },
+      {
+        model: Audience,
+        as: "audience",
+      },
+      {
+        model: Group,
+        as: "group",
+      },
+      {
+        model: Address,
+        as: "address",
+      },
+    ],
+  }).then((event) => {
+    res.json(event);
+  });
+});
+
+app.get("/api/events/tags", (req, res) => {
+  Tag.findAll().then((tags) => {
+    res.json(tags);
+  });
+});
+
+app.get("/api/events/tags/:id", (req, res) => {
+  Tag.findByPk(req.params.id).then((tag) => {
+    res.json(tag);
+  });
+});
+
+app.get("/api/events/accessTypes", (req, res) => {
+  AccessType.findAll().then((accessTypes) => {
+    res.json(accessTypes);
+  });
+});
+
+app.get("/api/events/accessTypes/:id", (req, res) => {
+  AccessType.findByPk(req.params.id).then((accessType) => {
+    res.json(accessType);
+  });
+});
+
+app.get("/api/events/priceTypes", (req, res) => {
+  PriceType.findAll().then((priceTypes) => {
+    res.json(priceTypes);
+  });
+});
+
+app.get("/api/events/priceTypes/:id", (req, res) => {
+  PriceType.findByPk(req.params.id).then((priceType) => {
+    res.json(priceType);
+  });
+});
+
+app.get("/api/events/audiences", (req, res) => {
+  Audience.findAll().then((audiences) => {
+    res.json(audiences);
+  });
+});
+
+app.get("/api/events/audiences/:id", (req, res) => {
+  Audience.findByPk(req.params.id).then((audience) => {
+    res.json(audience);
+  });
+});
+
+app.get("/api/events/groups", (req, res) => {
+  Group.findAll().then((groups) => {
+    res.json(groups);
+  });
+});
+
+app.get("/api/events/groups/:id", (req, res) => {
+  Group.findByPk(req.params.id).then((group) => {
+    res.json(group);
+  });
+});
+
+app.get("/api/events/addresses", (req, res) => {
+  Address.findAll().then((addresses) => {
+    res.json(addresses);
+  });
+});
+
+app.get("/api/events/addresses/:id", (req, res) => {
+  Address.findByPk(req.params.id).then((address) => {
+    res.json(address);
   });
 });
 
