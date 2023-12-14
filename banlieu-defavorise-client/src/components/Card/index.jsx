fa83164cb6c5ebfcd4d165dac4e19dc64205f4a2
dan-arki <100216647+dan-arki@users.noreply.github.com>
2023-12-14 15:05:03 +0100

    title from api added

--- banlieu-defavorise-client/src/components/Card/index.jsx
+++ banlieu-defavorise-client/src/components/Card/index.jsx
@@ -1,24 +1,27 @@
 import Tag from "../Tag";
 
-function Card() {
+function Card({ eventData }) {
+  // Utilisation des données reçues via les props
+  const { title, image, date } = eventData;
+
   return (
     <div className="flex flex-column">
       <div>
         <img
           className="w-78 h-96 mb-4 rounded-3xl border-solid border-8 border-primary-blue border-opacity-20"
-          src="images/testImg.jpg"
+          src={image} // Assurez-vous d'avoir une propriété image dans vos données d'événement
           alt=""
         />
         <div className="flex flex-col gap-4">
           <Tag></Tag>
           <div className="flex flex-col gap-0.5">
             <h2 className="text-xl text-textColor font-bold">
-              Exposition Haussmann en capitale
+              {title} {/* Utilisation du titre de l'événement */}
             </h2>
             <div className="flex flex-row gap-2">
               <img src="icons/iconCalendar.svg" alt="" />
               <span className="text-sm text-secondaryRed font-medium">
-                2 novembre - 31 décembre 2023
+                {date} {/* Utilisation de la date de l'événement */}
               </span>
             </div>
           </div>
